796ff22450e1399cda782063e151b7f68c2d181c
tnyorai <shimazu.toru@tis.co.jp>
2021-08-05 09:54:48 +0900

    #39 Amazon EFS作成用tfファイル追加

---
+++ terrafrom/environment/amazon-efs/amazon-efs.tf
@@ -0,0 +1,38 @@
+terraform {
+  required_version = ">= 0.15.4"
+
+  backend "s3" {
+    bucket         = "PJ-NAME-tfstate"
+    key            = "efs/terraform.tfstate"
+    encrypt        = true
+    dynamodb_table = "PJ-NAME-tfstate-lock"
+    region         = "REGION"
+  }
+}
+
+provider "aws" {
+  region = "REGION"
+}
+
+locals {
+  # common parameter
+  pj    = "PJ-NAME"
+  env   = "ENVIRONMENT"
+  owner = "OWNER"
+
+  tags = {
+    pj    = local.pj
+    env   = local.env
+    owner = local.owner
+  }
+}
+
+module "amazon-efs" {
+
+  source = "../../module/efs.tf"
+
+  # Common
+  base_name = "${local.pj}-${local.env}"
+  tags      = local.tags
+
+}
