b61b1fe2082811ca3fd09654a9fe4f2973985848
tnyorai <shimazu.toru@tis.co.jp>
2021-08-05 11:35:17 +0900

    #39 add:efs作成のためのoutputパラメータ追加

--- terrafrom/module/docker-swarm/output.tf
+++ terrafrom/module/docker-swarm/output.tf
@@ -1,3 +1,14 @@
-output "mount-ip" {
-  value = aws_efs_mount_target.this.ip_address
+output "public_subnet_az" {
+  description = "Public Subnet を作成したAZ"
+  value       = aws_subnet.pub-sub.availability_zone
+}
+
+output "pubulic_subnet_id" {
+  description = "Public SubnetのID"
+  value       = aws_subnet.pub-sub.id
+}
+
+output "sg_id" {
+    description = "nodeに割り当てたSGのID"
+    value = aws_security_group.swarm-node-sg.id
 }
