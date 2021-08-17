650f30cc904ff302a4227470c6593d7bdefa081c
tnyorai <shimazu.toru@tis.co.jp>
2021-08-17 09:54:58 +0900

    #44: fix:docker-swarmモジュールのbackend設定を削除

--- terrafrom/environment/docker-swarm/docker-swarm.tf
+++ terrafrom/environment/docker-swarm/docker-swarm.tf
@@ -1,9 +1,11 @@
 provider "aws" {
+  # 環境を構築するリージョンを指定します。
   region = "REGION"
 }
 
 locals {
   # common parameter
+  ## 各リソースの名称やタグ情報に使用するパラメータを指定します。
   pj    = "PJ-NAME"
   env   = "ENVIRONMENT"
   owner = "OWNER"
@@ -15,27 +17,34 @@ locals {
   }
 
   # Network
-  ## VPC
+  ## VPCに割り当てるCIDRを指定します。
   vpc_cidr = "10.100.0.0/16"
-  ## subnet
+  ## subnetに割り当てるCIDRを指定します。
   subnet_cidr = "10.100.0.0/24"
 
   # EC2
-  nodes         = ["manager0", "manager1", "manager2", "worker0", "worker1"] # リストした名前のノードを作成
+  ## 作成するノードの一覧です。リストした名前のEC2を作成します。
+  nodes = ["manager0", "manager1", "manager2", "worker0", "worker1"]
+  ## 作成するEC2のインスタンスタイプを指定します。
   instance_type = "t3.medium"
-  key_name      = "swarm" # インスタンスのキーペア。あらかじめ作成が必要
+  ## EC2に割り当てるキーペアを指定します。あらかじめ作成が必要です。
+  key_name = "key_pair_name"
 
   # SecurityGroup
-  allow_ssh_cidrs        = [] # 各ノードにインターネット経由でSSH接続する場合に送信元グローバルIPを指定する。どこからも許可しない場合は空配列を指定する。
-  allow_docker_api_cidrs = [] # 各ノードにインターネット経由でSSH接続する場合に送信元グローバルIPを指定する。どこからも許可しない場合は空配列を指定する。
+  ## 各ノードにインターネット経由でSSH接続する場合に送信元グローバルIPを指定します。
+  ## どこからも許可しない場合は空配列を指定する。
+  allow_ssh_cidrs = []
+  ## 各ノードにインターネット経由でdocker APIを利用する場合に送信元グローバルIPを指定します。
+  ## どこからも許可しない場合は空配列を指定する。
+  allow_docker_api_cidrs = []
 
 
   # ClowdWatch
-  auto_start          = true                       # trueにするとEC2の自動起動をスケジュール
+  ## 作成するEC2に自動起動/自動停止のスケジュールを設定します。
+  auto_start          = true                       # trueにするとEC2の自動起動をスケジュール設定します。
   auto_start_schedule = "cron(06 6 ? * MON-FRI *)" # 日本時間で平日09:00の指定
-  auto_stop           = true                       # trueにするとEC2の自動停止をスケジュール
+  auto_stop           = true                       # trueにするとEC2の自動停止をスケジュール設定します。
   auto_stop_schedule  = "cron(04 6 ? * MON-FRI *)" # 日本時間で平日19:00の指定
-
 }
 
 module "docker-swarm" {
