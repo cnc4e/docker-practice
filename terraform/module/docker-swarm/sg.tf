4137dbda6195a4d90d5f3976dfd7b296881de790
tnyorai <shimazu.toru@tis.co.jp>
2021-09-02 15:25:31 +0900

    #52 sg設定を修正

--- terraform/module/docker-swarm/sg.tf
+++ terraform/module/docker-swarm/sg.tf
@@ -44,44 +44,14 @@ resource "aws_security_group_rule" "docker-api" {
   description       = "allow Docker API"
 }
 
-resource "aws_security_group_rule" "self-2377" {
+resource "aws_security_group_rule" "self-any" {
   type              = "ingress"
-  from_port         = 2377
-  to_port           = 2377
-  protocol          = "tcp"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
   security_group_id = aws_security_group.swarm-node-sg.id
   self              = true
-  description       = "cluster management"
-}
-
-resource "aws_security_group_rule" "self-7946-tcp" {
-  type              = "ingress"
-  from_port         = 7946
-  to_port           = 7946
-  protocol          = "tcp"
-  security_group_id = aws_security_group.swarm-node-sg.id
-  self              = true
-  description       = "node communication TCP"
-}
-
-resource "aws_security_group_rule" "self-7946-udp" {
-  type              = "ingress"
-  from_port         = 7946
-  to_port           = 7946
-  protocol          = "udp"
-  security_group_id = aws_security_group.swarm-node-sg.id
-  self              = true
-  description       = "node communication UDP"
-}
-
-resource "aws_security_group_rule" "self-4789" {
-  type              = "ingress"
-  from_port         = 4789
-  to_port           = 4789
-  protocol          = "udp"
-  security_group_id = aws_security_group.swarm-node-sg.id
-  self              = true
-  description       = "overlay network"
+  description       = "internal"
 }
 
 resource "aws_security_group_rule" "self-esp" {
@@ -92,14 +62,4 @@ resource "aws_security_group_rule" "self-esp" {
   security_group_id = aws_security_group.swarm-node-sg.id
   self              = true
   description       = "overlay network encripted"
-}
-
-resource "aws_security_group_rule" "zabbix" {
-  type              = "ingress"
-  from_port         = 10050
-  to_port           = 10051
-  protocol          = "tcp"
-  security_group_id = aws_security_group.swarm-node-sg.id
-  self              = true
-  description       = "zabbix"
 }
